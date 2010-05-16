a5326a1b3b8ec7bd7b1195b1ffb5d20cae7331c9
Sean Dague <sean@dague.net>
2010-05-16 11:19:59 -0400

    Adding in about view

---
+++ src/net/dague/astro/About.java
@@ -0,0 +1,12 @@
+package net.dague.astro;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class About extends Activity {
+	protected void onCreate(Bundle savedInstanceState) {
+		   super.onCreate(savedInstanceState);
+		   setContentView(R.layout.about);
+		}
+
+}
--- src/net/dague/astro/JovianSpiral.java
+++ src/net/dague/astro/JovianSpiral.java
@@ -6,47 +6,62 @@ import net.dague.astro.util.JovianPoints;
 import net.dague.astro.util.SolarCalc;
 import net.dague.astro.util.SolarSim;
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.DialogInterface.OnClickListener;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
-public class JovianSpiral extends Activity {
+public class JovianSpiral extends Activity implements OnClickListener {
 	
 	AsyncTask<SolarCalc,View,Void> jc;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.i("IO", "got past create");
-        
-        // TODO: remove once progressive rendering works
 
         JovianGraphView jgv = new JovianGraphView(this);
 
-        // SimData d = new SimData(this);
-        // d.deleteAll();
         SolarSim sim = new SolarSim(this);
-        // JovianPoints jp = sim.getMoonPoints(JovianGraphView.startTime(), 120, true);
         
         new Thread(new JovianCalculator(sim, jgv, JovianGraphView.startTime(), 120)).start();
-//        if(!jp.complete()) {
-//        		this.jc = new JovianCalculator().execute(new SolarCalc(sim, JovianGraphView.startTime(), 120, jgv));
-//        	}
-//      }
         
     	setContentView(jgv);
         
         jgv.requestFocus();
     
-//        SolarSim s = new SolarSim();
-//        JovianMoons jv = s.getMoons(System.currentTimeMillis());
-//        Log.i("IO", jv.toString());
-//        setContentView(R.layout.main);
     }
 //    protected void onPause() {
 //    	super.onPause();
 //    }
+	@Override
+	public void onClick(DialogInterface dialog, int arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.about:
+			startActivity(new Intent(this, About.class));
+		    return true;
+		}
+		return false;
+	}
+
+	
+	public boolean onCreateOptionsMenu(Menu menu) {
+	   super.onCreateOptionsMenu(menu);
+	   MenuInflater inflater = getMenuInflater();
+	   inflater.inflate(R.menu.menu, menu);
+	   return true;
+	}
+
     
 //    protected void onSaveInstanceState(Bundle  outState) {
 //    	if (this.jc != null && this.jc.getStatus() != AsyncTask.Status.FINISHED) {
