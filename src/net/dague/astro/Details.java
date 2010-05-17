09b70d36f96d4b7f06bc04e9c0595ec0af86d161
Sean Dague <sean@dague.net>
2010-05-17 08:22:21 -0400

    added basic Jupiter dialog poppup

--- src/net/dague/astro/Details.java
+++ src/net/dague/astro/Details.java
@@ -2,13 +2,27 @@ package net.dague.astro;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Details extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.details);
+		setDetails();
+	}
+	
+	private void setDetails() {
 		Bundle extras = getIntent().getExtras();
 		String body = extras.getString("net.dague.astro.DetailBody");
-		setContentView(R.layout.details);
 		setTitle(body);
+		
+		TextView t = (TextView)findViewById(R.id.details_content);
+		ImageView i = (ImageView)findViewById(R.id.details_image);
+
+		if (body.equals("Jupiter")) {
+			t.setText(R.string.jupiter_details);
+			i.setImageResource(R.drawable.jupiter);
+		}
 	}
 }
