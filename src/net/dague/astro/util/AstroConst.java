82c958d8b4939ddeaeaef6d0e2efc7bdea099c44
Sean Dague <sean@dague.net>
2010-05-20 08:50:54 -0400

    License statements added everywhere for GPLv3.  This doesn't quite give you enough to build because of the missing native builder, but because of the way ndk works I'm not sure of a clean way to deal with that yet.

--- src/net/dague/astro/util/AstroConst.java
+++ src/net/dague/astro/util/AstroConst.java
@@ -1,3 +1,22 @@
+/*
+  Copyright 2010 Sean Dague
+
+  This file is part of Where is Io
+
+  Where is Io is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  
+  Where is Io is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with Where is Io.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package net.dague.astro.util;
 
 public class AstroConst {
