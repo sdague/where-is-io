d7f58a64dc0743411d3a480afc40ff7028205fab
Sean Dague <sean@dague.net>
2010-10-03 09:05:11 -0400

    got the Diarama working the way I expected

--- src/net/dague/astro/views/Diarama.java
+++ src/net/dague/astro/views/Diarama.java
@@ -63,7 +63,12 @@ public class Diarama extends View {
 		}  
 		Log.i("GPS", "Lat: " + gps[0] + ", Lon: " + gps[1]);
 		return gps;  
-	}  
+	}
+	
+	protected void paintRise(Canvas c, int body) {
+		
+		double now = TimeUtil.JDfloor(TimeUtil.mils2JD(System.currentTimeMillis()));
+	}
 	
 	protected void onDraw(Canvas c) {
 		
@@ -96,6 +101,16 @@ public class Diarama extends View {
 		r.left = -w/4;
 		r.right = w + w/4;
 		
+
+		paintBody(c, SolarSim.NEPTUNE, R.drawable.neptune_icon, p);
+		paintBody(c, SolarSim.URANUS, R.drawable.uranus_icon, p);
+		paintBody(c, SolarSim.SATURN, R.drawable.saturn_icon, p);
+		paintBody(c, SolarSim.JUPITER, R.drawable.jupiter_icon, p);
+		paintBody(c, SolarSim.MARS, R.drawable.mars_icon, p);
+		paintBody(c, SolarSim.VENUS, R.drawable.venus_icon, p);
+		paintBody(c, SolarSim.MERCURY, R.drawable.mercury_icon, p);
+		paintBody(c, SolarSim.SUN, R.drawable.sun_icon, p);
+		
 		BitmapDrawable d = (BitmapDrawable)(getContext().getResources().getDrawable(R.drawable.sun_icon));
 		Bitmap sunBitmap = d.getBitmap();
 		
@@ -103,9 +118,61 @@ public class Diarama extends View {
 		
 
 		// c.drawCircle(w/2, h/2, h/4, p);
-		c.drawBitmap(sunBitmap, src, dst, p);
+		// c.drawBitmap(sunBitmap, src, dst, p);
 		c.drawArc(r, 180, 180, false, p);
+		
 
+	}
+
+	private void paintBody(Canvas c, int body, int icon, Paint p) {
+		double realnow = TimeUtil.mils2JD(System.currentTimeMillis());
+		double now = TimeUtil.JDfloor(realnow);
+		double stage = 0.0;
+
+		int h = this.getHeight();
+		int w = this.getWidth();
+		
+		h -= h/5; 
+		
+		double rise = rs.riseTime(body, now);
+		double set = rs.setTime(body, now);
+		if (rise > set) {
+			rise -= 1.0;
+		}
+		
+		if(realnow > rise && realnow < set) {
+			stage = (realnow - rise) / (set - rise);
+		} else {
+			return;
+		}
+		
+		Log.i("DIARAMA", "Body:" + body + " - Stage: " + stage + ", R: " + rise + ", S: " + set);
+
+		Log.i("DIARAMA", "h:" + h + ", w:" + w);
+
+		BitmapDrawable d = (BitmapDrawable)(getContext().getResources().getDrawable(icon));
+		Bitmap bodyBitmap = d.getBitmap();
+		
+		// Now we need to compute where things actually are on the diarama
+		
+		Rect src = new Rect(0, 0, bodyBitmap.getWidth(), bodyBitmap.getHeight());
+		int radius = bodyBitmap.getWidth() / 2;
+		
+		double radians = Math.PI - (stage * Math.PI);
+		Log.i("DIARAMA", "Radians: " + radians);
+		
+		// This looks really complicated, and it sort of is.  There was a lot of manual
+		// tuning with stage = 0.0, 0.5, and 1.0 to make things land in sane places.
+		
+		double x = Math.cos(radians) * (w-radius)/2 + (w-(radius*2))/2 + radius;
+		double y = h - Math.sin(radians) * h + radius;
+		Log.i("DIARMA", "Sin: " + Math.sin(radians));
+
+		RectF dst = new RectF((float)(x-radius),(float)(y-radius),(float)(x+radius),(float)(y+radius));
+		c.drawBitmap(bodyBitmap, src, dst, p);
+		
+		// TODO Auto-generated method stub
+		
 	}
 
 }
